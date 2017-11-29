26467e2306850b99a6a7578e2043e93571dd207d
adang <amber.dang@morningstar.com>
2017-11-29 17:57:33 +0800

    Use V1 api

--- mainEntry.py
+++ mainEntry.py
@@ -5,17 +5,18 @@ from Acc import btfAccount
 import hmac
 import hashlib
 import json
+import base64
 
-def getAuthHeader(loginName, loginPasswd, srvPath, rawBody):
-    tmpNonce = str(int(time.time() * 10000000))
-    secSignature = "/api/" + srvPath + tmpNonce + rawBody
-    tmpH = hmac.new(loginPasswd.encode(), secSignature.encode(), hashlib.sha384)
+def getAuthHeader(loginName, loginPasswd, rawBody):
+    body64str = str(base64.b64encode(rawBody.encode('utf-8')),'utf-8')
+    tmpH = hmac.new(loginPasswd.encode(),body64str.encode(), hashlib.sha384)
     secSignature = tmpH.hexdigest()
     tmpHeader = {
-        "bfx-nonce": tmpNonce,
-        "bfx-apikey": loginName,
-        "bfx-signature": secSignature,
-        "content-type": "application/json"
+        "X-BFX-APIKEY": loginName,
+        "X-BFX-payload": body64str,
+        "X-BFX-SIGNATURE": secSignature,
+        'Content-Type':'application/json',
+        'Accept':'application/json'
     }
     return tmpHeader
 
@@ -36,60 +37,28 @@ loginName = str(secStr['APIK'])
 passName = str(secStr['APIS'])
 
 
-#public info test: test the interface of get BitCoin-US Dollar bid information
-tmpSrvName = 'ticker/tBTCUSD'
+tmpBaseUrl = 'https://api.bitfinex.com'
+tmpSrvName = ''
 tmpParas = ''
-tmpUrl = 'https://api.bitfinex.com/v2/%s/%s' % (tmpSrvName, tmpParas)
-tmpResp = requests.get(tmpUrl)
+
+#public info test: Get Ticket
+tmpSrvName = '/v1/pubticker/btcusd'
+tmpParas = ''
+tmpResp = requests.get(tmpBaseUrl + tmpSrvName + tmpParas)
 logging.debug(tmpResp.text)
-jsonObj = tmpResp.json()
-tmpObj = {
-    'BID': jsonObj[0],
-    'BID_SIZE': jsonObj[1],
-    'ASK': jsonObj[2],
-    'ASK_SIZE': jsonObj[3],
-    'DAILY_CHANGE': jsonObj[4],
-    'DAILY_CHANGE_PERC': jsonObj[5],
-    'LAST_PRICE': jsonObj[6],
-    'VOLUME': jsonObj[7],
-    'HIGH': jsonObj[8],
-    'LOW': jsonObj[9]
-}
+tmpObj = tmpResp.json()
 logging.debug(tmpObj)
 
-#Authenticated information test, include: wallet, order history
-#Authenticated information need authenticate headers.
-tmpParas = ''
-tmpBaseUrl = 'https://api.bitfinex.com/'
-tmpSrvPath = 'v2/auth/r/wallets'
-jsonBody = {}
-rawBody = json.dumps(jsonBody)
-tmpHeader = getAuthHeader(loginName, passName, tmpSrvPath, rawBody)
-tmpResp = requests.post(tmpBaseUrl+tmpSrvPath+tmpParas, headers=tmpHeader, data=rawBody, verify=True)
-logging.debug(tmpResp.text)
-jsonObj = tmpResp.json()
-tmpObj = {
-    'BTC': {
-        'BALANCE':jsonObj[0][2],
-        'UNSETTLED_INTEREST':jsonObj[0][3],
-        'BALANCE_AVAILABLE':jsonObj[0][4]
-    },
-    'USD': {
-        'BALANCE':jsonObj[1][2],
-        'UNSETTLED_INTEREST':jsonObj[1][3],
-        'BALANCE_AVAILABLE':jsonObj[1][4]
-    }
+#Authenticated information test: Wallets Balances
+tmpSrvName = '/v1/balances'
+tmpNonce = str(int(time.time() * 10000000))
+jsonBody = {
+    'request': tmpSrvName,
+    'nonce': tmpNonce
 }
-
-
-
-tmpParas = '?start=600000&end=1000&limit=10'
-tmpBaseUrl = 'https://api.bitfinex.com/'
-tmpSrvPath = 'v2/auth/r/orders/tBTCUSD/hist'
-jsonBody = {}
 rawBody = json.dumps(jsonBody)
-tmpHeader = getAuthHeader(loginName, passName, tmpSrvPath, rawBody)
-tmpResp = requests.post(tmpBaseUrl+tmpSrvPath+tmpParas, headers=tmpHeader, data=rawBody, verify=True)
+tmpHeader = getAuthHeader(loginName, passName, rawBody)
+tmpResp = requests.post(tmpBaseUrl+tmpSrvName, headers=tmpHeader, data=rawBody, verify=True)
 logging.debug(tmpResp.text)
 jsonObj = tmpResp.json()
 
